c7f206784f1d25aa2342c14806850e9b70b4c515
Maulik-11 <maulikzambad@gmail.com>
2026-02-25 13:05:31 +0530

    yes

--- spam-email-verifier/src/models/email.ts
+++
@@ -1,9 +0,0 @@
-export class Email {
-    address: string;
-    status: string;
-
-    constructor(address: string, status: string) {
-        this.address = address;
-        this.status = status;
-    }
-}
